e8d039add3a0ed64aaa697f3eca11d6ea546eb85
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2024-06-27 11:10:18 -0400

    Create top-bar

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatchDetailComponent } from '../match-detail/match-detail.component';
+import { MatchListComponent } from '../match-list/match-list.component';
+import { TopBarComponent } from '../top-bar/top-bar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, MatchDetailComponent, MatchListComponent, TopBarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
+  sidenavOpened : boolean = true;
+  
+  toggleSidenav() {
+    this.sidenavOpened = !this.sidenavOpened;
+  }
 }
---
+++ src/app/components/top-bar/top-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-top-bar',
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, RouterLink],
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.scss'
+})
+export class TopBarComponent {
+  title = "Bracket Viewer"
+
+  @Output() sidenavEmitter : EventEmitter<any> = new EventEmitter();
+
+  toggleSidenav() {
+    this.sidenavEmitter.emit();
+  }
+}
