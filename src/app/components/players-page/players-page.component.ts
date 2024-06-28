2fca8de83eead8291cead7bf1899cf7b1860a2d4
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2024-06-28 09:39:25 -0400

    Add paginator

--- src/app/components/players-page/players-page.component.ts
+++ src/app/components/players-page/players-page.component.ts
@@ -1,47 +1,124 @@
 import { Component } from '@angular/core';
 import { Player } from '../../model/player';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { PlayerDetailComponent } from '../player-detail/player-detail.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-players-page',
   standalone: true,
-  imports: [NgFor, MatCardModule, MatButtonModule, PlayerDetailComponent],
+  imports: [
+    NgFor,
+    MatCardModule,
+    MatButtonModule,
+    PlayerDetailComponent,
+    MatPaginatorModule,
+    NgIf
+  ],
   templateUrl: './players-page.component.html',
-  styleUrl: './players-page.component.scss'
+  styleUrl: './players-page.component.scss',
 })
 export class PlayersPageComponent {
-
-  selectedPlayer? : Player;
-
-  players : Player[] = [
+  players: Player[] = [
     {
       id: 1,
       name: 'John',
       wins: 0,
       losses: 0,
-      city: "Orlando",
-      state: "Florida",
-      role: "Closer",
-      address: "123 Park Way",
-      picture: '../../../assets/no-image.jpg'
+      city: 'Orlando',
+      state: 'Florida',
+      role: 'Closer',
+      address: '123 Park Way',
+      picture: '../../../assets/no-image.jpg',
     },
     {
       id: 2,
       name: 'Sue',
       wins: 0,
       losses: 0,
-      city: "Buffalo",
-      state: "New York",
-      role: "Striker",
-      address: "456 Park Way",
-      picture: '../../../assets/no-image.jpg'
-    }
-  ]
+      city: 'Buffalo',
+      state: 'New York',
+      role: 'Striker',
+      address: '456 Park Way',
+      picture: '../../../assets/no-image.jpg',
+    },
+    {
+      id: 3,
+      name: 'Jack',
+      wins: 0,
+      losses: 0,
+      city: 'Denver',
+      state: 'Colorado',
+      role: 'Point Guard',
+      address: '789 Park Way',
+      picture: '../../../assets/no-image.jpg',
+    },
+    {
+      id: 4,
+      name: 'Jill',
+      wins: 0,
+      losses: 0,
+      city: 'Miami',
+      state: 'Florida',
+      role: 'Wide Tackle',
+      address: '135 Hill Rd',
+      picture: '../../../assets/no-image.jpg',
+    },
+    {
+      id: 5,
+      name: 'Matt',
+      wins: 0,
+      losses: 0,
+      city: 'Dallas',
+      state: 'Texas',
+      role: 'Blocker',
+      address: '246 Hill Rd',
+      picture: '../../../assets/no-image.jpg',
+    },
+    {
+      id: 6,
+      name: 'Mary',
+      wins: 0,
+      losses: 0,
+      city: 'Seattle',
+      state: 'Washington',
+      role: 'Pitcher',
+      address: '357 Hill Rd',
+      picture: '../../../assets/no-image.jpg',
+    },
+    {
+      id: 7,
+      name: 'Peter',
+      wins: 0,
+      losses: 0,
+      city: 'Columbus',
+      state: 'Ohio',
+      role: 'Kicker',
+      address: '468 Hill Rd',
+      picture: '../../../assets/no-image.jpg',
+    },
+    {
+      id: 8,
+      name: 'Sarah',
+      wins: 0,
+      losses: 0,
+      city: 'Atlanta',
+      state: 'Georgia',
+      role: 'Water Girl',
+      address: '685 Curvy St',
+      picture: '../../../assets/no-image.jpg',
+    },
+  ];
 
-  selectPlayer(player : Player) {
+  selectedPlayer?: Player;
+  pageIndex : number = 0;
+  pageSize = 5;
+  playersPage = this.players.slice(0, this.pageSize);
+
+
+  selectPlayer(player: Player) {
     this.selectedPlayer = structuredClone(player);
   }
 
@@ -49,8 +126,8 @@ export class PlayersPageComponent {
     this.selectedPlayer = undefined;
   }
 
-  saveChanges($event : Player) {
-    const toUpdate = this.players.filter(p => p.id == $event.id)[0]
+  saveChanges($event: Player) {
+    const toUpdate = this.players.filter((p) => p.id == $event.id)[0];
     if (toUpdate) {
       toUpdate.name = $event.name;
       toUpdate.wins = $event.wins;
@@ -63,9 +140,20 @@ export class PlayersPageComponent {
     }
   }
 
-  deletePlayer($event : Player) {
-    console.log(this.players);
-    console.log($event);
-    this.players = this.players.filter(t => t.id != $event.id);
+  deletePlayer($event: Player) {
+    this.players = this.players.filter((t) => t.id != $event.id);
+    if (this.pageSize * this.pageIndex >= this.players.length) this.pageIndex--;
+    this.renderSlice();
+  }
+
+  onPageChange($event : any) {
+    this.cancelSelection();
+    this.pageIndex = $event.pageIndex;
+    this.renderSlice();
+  }
+
+  renderSlice() {
+    const start = this.pageIndex * this.pageSize;
+    this.playersPage = this.players.slice(start, start + this.pageSize);
   }
 }
